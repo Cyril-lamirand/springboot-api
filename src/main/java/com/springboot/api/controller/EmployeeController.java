c727e7b8f364c8124ede1be63311ccb9b92a2c00
Cyril-lamirand <contact.cyril1@gmail.com>
2021-08-13 01:37:08 +0200

    Update EmployeeController.java

--- src/main/java/com/springboot/api/controller/EmployeeController.java
+++ src/main/java/com/springboot/api/controller/EmployeeController.java
@@ -1,11 +1,18 @@
 package com.springboot.api.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.springboot.api.model.Employee;
 import com.springboot.api.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -13,6 +20,36 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * Create - Add a new employee
+     * @param employee An object employee
+     * @return The employee object saved
+     */
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+
+    /**
+     * Read - Get one employee
+     * @param id The id of the employee
+     * @return An Employee object full filled
+     */
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        if(employee.isPresent()) {
+            return employee.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
@@ -27,9 +64,7 @@ public class EmployeeController {
     @PutMapping("/employee/{id}")
     public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
         Optional<Employee> e = employeeService.getEmployee(id);
-
-        if (e.isPresent()) {
-
+        if(e.isPresent()) {
             Employee currentEmployee = e.get();
 
             String firstName = employee.getFirstname();
@@ -50,11 +85,11 @@ public class EmployeeController {
             }
             employeeService.saveEmployee(currentEmployee);
             return currentEmployee;
-
         } else {
             return null;
         }
     }
+
 
     /**
      * Delete - Delete an employee
